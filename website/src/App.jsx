e9d224602aa6582a7c8857f2ea4907828e32afad
Martin <martin.ng@live.com>
2025-04-02 13:40:46 +0300

    Add background blobs

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -6,6 +6,19 @@ export default function App() {
     <div className="App">
       <Navbar />
       <Hero />
+
+      <div
+        aria-hidden="true"
+        className="fixed hidden dark:md:block dark:opacity-70 -bottom-[40%] -left-[20%] z-0"
+      >
+        <img src="./gradients/left-blob.png" alt="" />
+      </div>
+      <div
+        aria-hidden="true"
+        className="fixed hidden dark:md:block dark:opacity-70 -top-[80%] -right-[60%] 2xl:-top-[60%] 2xl:-right-[45%] z-0 rotate-12"
+      >
+        <img src="./gradients/right-blob.png" alt="" />
+      </div>
     </div>
   );
 }
