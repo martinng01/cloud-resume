345737e10955ec9f52ca5cff160a0e350b3cf69f
Martin <martin.ng@live.com>
2025-04-02 13:41:53 +0300

    Fix navbar in place

--- website/src/sections/Navbar.jsx
+++ website/src/sections/Navbar.jsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar fixed bg-base-100">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">My Portfolio</a>
       </div>
