44434f47806c9bc173ca0e19c985774a3b0859ad
Martin <martin.ng@live.com>
2025-04-07 18:29:46 +0300

    Add work section

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,21 +1,23 @@
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
+import Work from "./sections/Work";
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Work />
 
       <div
         aria-hidden="true"
-        className="fixed hidden dark:md:block dark:opacity-70 -bottom-[40%] -left-[20%] z-0"
+        className="fixed hidden dark:md:block dark:opacity-70 -bottom-[40%] -left-[20%] z-[-1]"
       >
         <img src="./gradients/left-blob.png" alt="" />
       </div>
       <div
         aria-hidden="true"
-        className="fixed hidden dark:md:block dark:opacity-70 -top-[80%] -right-[60%] 2xl:-top-[60%] 2xl:-right-[45%] z-0 rotate-12"
+        className="fixed hidden dark:md:block dark:opacity-70 -top-[80%] -right-[60%] 2xl:-top-[60%] 2xl:-right-[45%] z-[-1] rotate-12"
       >
         <img src="./gradients/right-blob.png" alt="" />
       </div>
--- website/src/sections/Work.jsx
+++ website/src/sections/Work.jsx
@@ -1,58 +1,124 @@
-import { Image } from "@heroui/image";
+import Markdown from "react-markdown";
 
-import { subtitle, title } from "@/components/primitives";
+const timelineData = [
+  {
+    company: "Ventas Bio",
+    role: "AI/ML Transformation Intern",
+    date: "Sep 2024 - Jan 2025",
+    description: [
+      "Worked on and improved the company's **knowledge retrieval chatbot**",
+      "Improved **information extraction** from documents and **reranking** methods, leading to a **70% improvement** in response accuracy",
+      "Developed an **evaluation pipeline** for automated benchmarking of LLMs, saving time spent on manual evaluation",
+      "Collaborated with and **communicated complex AI concepts** to cross-functional teams, ensuring alignment throughout the development process",
+      "**Fine-tuned and evaluated** a Whisper model using transcripts extracted through OCR to enhance local speech recognition capabilities, increasing call agent efficiency",
+    ],
+    skills: [
+      "RAG",
+      "Technical Communication",
+      "Ollama",
+      "Fine Tuning",
+      "LLM Evaluation",
+      "Information Retrieval",
+    ],
+  },
+  {
+    company: "A*STAR",
+    role: "Artificial Intelligence Intern",
+    date: "May 2024 - Aug 2024",
+    description: [
+      "Enhanced the accuracy of in-house translation models using RAG and LLMs by **17%**",
+      "Implemented a web application to benchmark the accuracy of lightweight translation systems",
+      "**Fine-tuned LLMs** to perform Text-Style Transfer",
+    ],
+    skills: [
+      "RAG",
+      "Technical Communication",
+      "Ollama",
+      "Fine Tuning",
+      "LLM Evaluation",
+      "Information Retrieval",
+    ],
+  },
+  {
+    company: "ANZElectric",
+    role: "Software Engineering Intern",
+    date: "Dec 2023 - May 2024",
+    description: [
+      "Created multiple cross-platform, user-facing applications using **Flutter** for various hardware devices",
+      "Designed the application's **UI/UX** using **Figma**, ensuring intuitive user experiences were delivered consistently",
+      "Implemented and managed **CI/CD pipelines** using GitHub Actions for seamless application deployment",
+    ],
+    skills: [
+      "RAG",
+      "Technical Communication",
+      "Ollama",
+      "Fine Tuning",
+      "LLM Evaluation",
+      "Information Retrieval",
+    ],
+  },
+  {
+    company: "A*STAR",
+    role: "Artificial Intelligence Intern",
+    date: "Jan 2022 - June 2022",
+    description: [
+      "**Scraped and preprocessed** millions of sentences from online sources to create high-quality datasets for training",
+      "**Trained and optimized hyperparameters** for a Neural Machine Translation (NMT) model, deploying the model using **Flask**",
+    ],
+    skills: [
+      "RAG",
+      "Technical Communication",
+      "Ollama",
+      "Fine Tuning",
+      "LLM Evaluation",
+      "Information Retrieval",
+    ],
+  },
+];
 
-function Timeline() {
+export default function Work() {
   return (
-    <div className="flex flex-col items-center justify-center w-full py-10">
-      <div className="grid grid-cols-3 gap-4 w-3/4 relative">
-        {/* Timeline line */}
-        <div className="absolute left-1/2 w-1 bg-gray-300 h-full transform -translate-x-1/2"></div>
-
-        {/* Timeline items */}
-        <div className="col-span-1 flex justify-end pr-4">
-          <div className="bg-white shadow-lg p-4 rounded-lg w-60">
-            <h3 className="font-bold">Job Title 1</h3>
-            <p>Company Name</p>
-            <p className="text-sm text-gray-500">2023 - Present</p>
-          </div>
-        </div>
-        <div className="col-span-1"></div>
-        <div className="col-span-1 flex justify-start pl-4">
-          <div className="bg-white shadow-lg p-4 rounded-lg w-60">
-            <h3 className="font-bold">Job Title 2</h3>
-            <p>Company Name</p>
-            <p className="text-sm text-gray-500">2022 - 2023</p>
-          </div>
-        </div>
-
-        <div className="col-span-1 flex justify-end pr-4">
-          <div className="bg-white shadow-lg p-4 rounded-lg w-60">
-            <h3 className="font-bold">Job Title 3</h3>
-            <p>Company Name</p>
-            <p className="text-sm text-gray-500">2021 - 2022</p>
-          </div>
-        </div>
-        <div className="col-span-1"></div>
-        <div className="col-span-1 flex justify-start pl-4">
-          <div className="bg-white shadow-lg p-4 rounded-lg w-60">
-            <h3 className="font-bold">Job Title 4</h3>
-            <p>Company Name</p>
-            <p className="text-sm text-gray-500">2020 - 2021</p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical mx-36">
+      {timelineData.map((entry, index) => TimelineItem({ entry, index }))}
+    </ul>
   );
 }
 
-export default function Hero() {
+function TimelineItem({ entry, index }) {
   return (
-    <div className="flex flex-row items-center justify-center h-screen w-full">
-      <div className="flex-5">
-        <span className={title()}>Work Experience</span>
+    <li key={index}>
+      {index !== 0 && <hr />}
+      <div className="timeline-middle">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="h-5 w-5"
+        >
+          <path
+            fillRule="evenodd"
+            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+            clipRule="evenodd"
+          />
+        </svg>
       </div>
-      <div className="flex-1">{Timeline()}</div>
-    </div>
+      <div
+        className={
+          index % 2 == 0
+            ? "timeline-start mb-10 md:text-end"
+            : "timeline-end md:mb-10"
+        }
+      >
+        <time className="font-mono italic">{entry.date}</time>
+        <div className="text-lg font-black">{entry.company}</div>
+        <div className="text-lg font-black">{entry.role}</div>
+        <ul className="prose prose-p:m-2">
+          {entry.description.map((point, index) => (
+            <li key={index}>{<Markdown>{point}</Markdown>}</li>
+          ))}
+        </ul>
+      </div>
+      {index !== timelineData.length - 1 && <hr />}
+    </li>
   );
 }
