fcf32da59c138bcaf176a75181ac4afcc042c51b
Martin <martin.ng@live.com>
2025-04-08 01:02:56 +0300

    Changed timeline style

---
+++ website/src/data/timelineData.js
@@ -0,0 +1,45 @@
+const timelineData = [
+  {
+    company: "Ventas Bio",
+    role: "AI/ML Transformation Intern",
+    date: "Sep 2024 - Jan 2025",
+    description: [
+      "Worked on and improved the company's **knowledge retrieval chatbot**",
+      "Improved **information extraction** from documents and **reranking** methods, leading to a **70% improvement** in response accuracy",
+      "Developed an **evaluation pipeline** for automated benchmarking of LLMs, saving time spent on manual evaluation",
+      "Collaborated with and **communicated complex AI concepts** to cross-functional teams, ensuring alignment throughout the development process",
+      "**Fine-tuned and evaluated** a Whisper model using transcripts extracted through OCR to enhance local speech recognition capabilities, increasing call agent efficiency",
+    ],
+  },
+  {
+    company: "A*STAR",
+    role: "Artificial Intelligence Intern",
+    date: "May 2024 - Aug 2024",
+    description: [
+      "Enhanced the accuracy of in-house translation models using RAG and LLMs by **17%**",
+      "Implemented a web application to benchmark the accuracy of lightweight translation systems",
+      "**Fine-tuned LLMs** to perform Text-Style Transfer",
+    ],
+  },
+  {
+    company: "ANZElectric",
+    role: "Software Engineering Intern",
+    date: "Dec 2023 - May 2024",
+    description: [
+      "Created multiple cross-platform, user-facing applications using **Flutter** for various hardware devices",
+      "Designed the application's **UI/UX** using **Figma**, ensuring intuitive user experiences were delivered consistently",
+      "Implemented and managed **CI/CD pipelines** using GitHub Actions for seamless application deployment",
+    ],
+  },
+  {
+    company: "A*STAR",
+    role: "Artificial Intelligence Intern",
+    date: "Jan 2022 - June 2022",
+    description: [
+      "**Scraped and preprocessed** millions of sentences from online sources to create high-quality datasets for training",
+      "**Trained and optimized hyperparameters** for a Neural Machine Translation (NMT) model, deploying the model using **Flask**",
+    ],
+  },
+];
+
+export default timelineData;
--- website/src/sections/Work.jsx
+++ website/src/sections/Work.jsx
@@ -1,86 +1,14 @@
 import Markdown from "react-markdown";
-
-const timelineData = [
-  {
-    company: "Ventas Bio",
-    role: "AI/ML Transformation Intern",
-    date: "Sep 2024 - Jan 2025",
-    description: [
-      "Worked on and improved the company's **knowledge retrieval chatbot**",
-      "Improved **information extraction** from documents and **reranking** methods, leading to a **70% improvement** in response accuracy",
-      "Developed an **evaluation pipeline** for automated benchmarking of LLMs, saving time spent on manual evaluation",
-      "Collaborated with and **communicated complex AI concepts** to cross-functional teams, ensuring alignment throughout the development process",
-      "**Fine-tuned and evaluated** a Whisper model using transcripts extracted through OCR to enhance local speech recognition capabilities, increasing call agent efficiency",
-    ],
-    skills: [
-      "RAG",
-      "Technical Communication",
-      "Ollama",
-      "Fine Tuning",
-      "LLM Evaluation",
-      "Information Retrieval",
-    ],
-  },
-  {
-    company: "A*STAR",
-    role: "Artificial Intelligence Intern",
-    date: "May 2024 - Aug 2024",
-    description: [
-      "Enhanced the accuracy of in-house translation models using RAG and LLMs by **17%**",
-      "Implemented a web application to benchmark the accuracy of lightweight translation systems",
-      "**Fine-tuned LLMs** to perform Text-Style Transfer",
-    ],
-    skills: [
-      "RAG",
-      "Technical Communication",
-      "Ollama",
-      "Fine Tuning",
-      "LLM Evaluation",
-      "Information Retrieval",
-    ],
-  },
-  {
-    company: "ANZElectric",
-    role: "Software Engineering Intern",
-    date: "Dec 2023 - May 2024",
-    description: [
-      "Created multiple cross-platform, user-facing applications using **Flutter** for various hardware devices",
-      "Designed the application's **UI/UX** using **Figma**, ensuring intuitive user experiences were delivered consistently",
-      "Implemented and managed **CI/CD pipelines** using GitHub Actions for seamless application deployment",
-    ],
-    skills: [
-      "RAG",
-      "Technical Communication",
-      "Ollama",
-      "Fine Tuning",
-      "LLM Evaluation",
-      "Information Retrieval",
-    ],
-  },
-  {
-    company: "A*STAR",
-    role: "Artificial Intelligence Intern",
-    date: "Jan 2022 - June 2022",
-    description: [
-      "**Scraped and preprocessed** millions of sentences from online sources to create high-quality datasets for training",
-      "**Trained and optimized hyperparameters** for a Neural Machine Translation (NMT) model, deploying the model using **Flask**",
-    ],
-    skills: [
-      "RAG",
-      "Technical Communication",
-      "Ollama",
-      "Fine Tuning",
-      "LLM Evaluation",
-      "Information Retrieval",
-    ],
-  },
-];
+import timelineData from "../data/timelineData";
 
 export default function Work() {
   return (
-    <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical mx-36">
-      {timelineData.map((entry, index) => TimelineItem({ entry, index }))}
-    </ul>
+    <div className="mx-36">
+      <h1 className="text-5xl font-bold my-24 text-center">Work Experience</h1>
+      <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+        {timelineData.map((entry, index) => TimelineItem({ entry, index }))}
+      </ul>
+    </div>
   );
 }
 
@@ -105,14 +33,16 @@ function TimelineItem({ entry, index }) {
       <div
         className={
           index % 2 == 0
-            ? "timeline-start mb-10 md:text-end"
-            : "timeline-end md:mb-10"
+            ? "timeline-start timeline-box mb-10 mx-6"
+            : "timeline-end timeline-box mb-10 mx-6"
         }
       >
         <time className="font-mono italic">{entry.date}</time>
         <div className="text-lg font-black">{entry.company}</div>
-        <div className="text-lg font-black italic">{entry.role}</div>
-        <ul className="prose prose-p:m-2">
+        <div className="text-lg font-black italic text-[#07BEB8]">
+          {entry.role}
+        </div>
+        <ul className="list-disc prose prose-p:m-2 px-4">
           {entry.description.map((point, index) => (
             <li key={index}>{<Markdown>{point}</Markdown>}</li>
           ))}
